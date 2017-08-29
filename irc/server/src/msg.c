240e52a16180c6148c3810c7e6177f0549e7f320
semanaz romain <romain.semanaz@epitech.eu>
2017-08-29 23:07:00 +0200

    ok

---
+++ irc/server/src/msg.c
@@ -0,0 +1,48 @@
+
+#include <string.h>
+#include <stdio.h>
+#include "irc.h"
+
+int		add_msg(t_client *client, char *msg, const char *nickname)
+{
+  t_msg		*new;
+  t_msg		*tmp;
+
+  if ((new = xmalloc(sizeof(*new))) == NULL)
+    return (EXIT_ERROR);
+  new->msg = msg;
+  new->next = NULL;
+  memset(new->from, 0, NICKNAME_MAX);
+  strncpy(new->from, nickname, NICKNAME_MAX);
+  if ((tmp = client->msgq) == NULL)
+    client->msgq = new;
+  else
+    {
+      while (tmp->next != NULL)
+	tmp = tmp->next;
+      tmp->next = new;
+    }
+  return (EXIT_SUCCESS);
+}
+
+t_msg		*get_msg(t_client *client)
+{
+  t_msg		*ret;
+
+  ret = client->msgq;
+  client->msgq = client->msgq->next;
+  return (ret);
+}
+
+void		send_msg(t_client *client)
+{
+  t_msg		*msg;
+
+  if (client->msgq != NULL)
+    {
+      msg = get_msg(client);
+      if (dprintf(client->fd, "FROM %s: %s", msg->from, msg->msg) < 0)
+	perror("Write failed (send_msg)\n");
+      free(msg);
+    }
+}
